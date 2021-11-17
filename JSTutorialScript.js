1cd183773fbce0da374ced44ef155430d47ea594
jmettam20 <78258956+jmettam20@users.noreply.github.com>
2021-11-17 13:38:56 +0000

    Add files via upload

---
+++ JSTutorialScript.js
@@ -0,0 +1,124 @@
+//in line comment 
+
+/*
+multi line comment 
+*/
+
+// Var can be used to declare a variable of any and all types 
+var myName = "Josh";
+var myName = 8;
+var myName = true;
+var myName = 6.9;
+var myName = null; 
+
+//let is only used in the scope it is declared e.g. can only be used in the function it is declared in
+let ourName = "Jeff";
+
+// const = constant variable (dosnt change)
+const pi = 3.14; 
+
+//Declares a variable
+var a; 
+
+//assigns variable 
+a = 12; 
+
+// Declares and assigns a varaible 
+var b = 8; 
+
+//console debugging 
+console.log("I am the debug",b); 
+
+//adding
+
+var sum = 10 + 10;
+
+console.log("Sum is", sum);
+
+var x = 5;
+var y = 1;
+
+var result = x+y;
+console.log("Adding",result);
+
+//subtracting
+
+var a = 10;
+var b = 5;
+
+var subResult = a-b; 
+
+console.log("Subtracting",subResult);
+
+//multiply
+
+var c = 2;
+var d = 10;
+
+var multiResult = c*d; 
+console.log("Multiplication", multiResult);
+
+//devision 
+
+var e = 50;
+var f = 2;
+
+var divResult = e/f;
+
+console.log("Division", divResult); 
+
+
+//increment 
+
+var incrament = 80;
+
+incrament++; 
+
+console.log("Incramnet",incrament);
+
+
+
+//decrement
+
+var decrement = 80;
+decrement--;
+
+console.log("Decrament",decrement)
+
+//remainder
+var remainder;
+remainder = 11 % 3;
+console.log(remainder);
+
+//adding to a var 
+
+var g = 12;
+
+g += 4;
+
+console.log(g);
+
+//sub from var
+
+h = 16;
+h -= 4;
+
+console.log(h);
+
+
+// multiply var
+
+l = 20;
+l *= 2;
+console.log(l);
+
+//div var
+
+m = 50;
+m /= 2; 
+console.log(m);
+
+///quote in quote
+
+var name = "Josh \"Bad at programming\" Mettam"
+console.log(name);
